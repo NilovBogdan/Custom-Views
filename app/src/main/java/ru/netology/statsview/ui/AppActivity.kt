fcb3d357fea20c9a61498e03549494c8a7c95acc
Nilov Bogdan <bnilov106@gmil.com>
2025-03-16 23:42:45 +0300

    Initial commit

--- app/src/main/java/ru/netology/statsview/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/statsview/ui/AppActivity.kt
@@ -1,10 +1,7 @@
 package ru.netology.statsview.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ru.netology.statsview.R
 
 class AppActivity : AppCompatActivity(R.layout.activity_main) {
